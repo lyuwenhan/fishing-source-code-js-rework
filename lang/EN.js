a7fe14cc74bcf2bea717a53a47bb47c3e515666b
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2026-04-17 16:10:22 -0400

    Update EN.js

--- lang/EN.js
+++ lang/EN.js
@@ -4,7 +4,7 @@ export default {
 		speed_name: ["slow mode", "normal mode", "instant mode"],
 		this_is: "This is ",
 		choose_speed: "Please choose output speed.",
-		skills: ["Please choose a starting skill", "1. Delicious Bait, 2. Rare Treasures, 3. Secure Safety Rope, 4. Powerful Cleaner, 5. Popeye the Sailor, 6. Long Line for Big Fish, 7. Poor Family", "Delicious Bait:", "    Hooking speed increases to 1.5×", "Rare Treasures:", "    Fish prices increase to 2×", "Secure Safety Rope:", "    Slip-off chance greatly reduced (50% → 10%)", "Powerful Cleaner:", "    Start with 1 cleaner, and its effectiveness is doubled", "Popeye the Sailor:", "    Casting and reeling speed increase to 2×", "Long Line for Big Fish:", "    Big fish chance increases (20% → 40%)", "Poor Family:", "    No additional bonuses"],
+		skills: ["Please choose a starting skill", "1. Delicious Bait, 2. Rare Treasures, 3. Secure Safety Rope, 4. Powerful Cleaner, 5. Popeye the Sailor, 6. Long Line for Big Fish, 7. Poor Family", "Delicious Bait:", "    Hooking speed increases to 1.5x", "Rare Treasures:", "    Fish prices increase to 2x", "Secure Safety Rope:", "    Slip-off chance greatly reduced (50% -> 10%)", "Powerful Cleaner:", "    Start with 1 cleaner, and its effectiveness is doubled", "Popeye the Sailor:", "    Casting and reeling speed increase to 2x", "Long Line for Big Fish:", "    Big fish chance increases (20% -> 40%)", "Poor Family:", "    No additional bonuses"],
 		exit: "exit",
 		press_enter_to_continue: "press Enter to continue"
 	},
